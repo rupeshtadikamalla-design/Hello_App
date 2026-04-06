dd99bf6a056afcbef78373f8a052d67823097529
rupeshtadikamalla-design <rupeshtadikamalla@gmail.com>
2026-04-06 09:47:12 +0530

    Add files via upload

---
+++ HelloApp_UC3.java
@@ -0,0 +1,20 @@
+/**
+ * HelloApp - UC3 - Display Hello with Command-Line Argument or Default Message
+ * @author Rupesh
+ * @version 3.0
+ */
+public class HelloApp_UC3 {
+    public static void main(String[] args) {
+        
+        
+        if (args.length > 0) {
+            String name = args[0];
+            
+            System.out.println("Hello " + name);
+        } else {
+            String defaultName = "World";
+            System.out.println("Hello " + defaultName);
+        }
+        
+    }
+}
